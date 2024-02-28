3181d12f318fa4da202726cc9dc39e693fc5b13f
Grant Davis <grantdavis303@gmail.com>
2024-02-28 12:54:21 -0700

    Class Updates and RSpec Test

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,7 @@ class Game
     attr_reader :game_id, :season, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue
     def initialize(game_id, season, away_team_id, home_team_id, away_goals, home_goals, venue)
         @game_id = game_id
-        @season = formatted_season(season)
+        @season = season #formatted_season(season)
         @away_team_id = away_team_id.to_i
         @home_team_id = home_team_id.to_i
         @away_goals = away_goals.to_i
@@ -27,16 +27,16 @@ class Game
         end
     end
 
-    def formatted_season(season)
-        formatted_season = {
-            start: season[0..3],
-            end: season[4..7]
-        }
+    # def formatted_season(season)
+    #     formatted_season = {
+    #         start: season[0..3],
+    #         end: season[4..7]
+    #     }
         
-        formatted_season
-    end
+    #     formatted_season
+    # end
 
-    def search_for_team_name(id)
-    end
+    # def search_for_team_name(id)
+    # end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,22 @@
-require './spec_helper'
+require './spec/spec_helper'
 
-describe Game do
+RSpec.describe Game do
 
+    it 'exists' do
+        new_game = Game.new(2012030221, 20122013, 3, 6, 2, 3, "Toyota Stadium")
+        expect(new_game).to be_an_instance_of Game
+    end
+    
+    it 'has attributes' do
+        new_game = Game.new(2012030221, 20122013, 3, 6, 2, 3, "Toyota Stadium")
+        
+        expect(new_game.game_id).to eq 2012030221
+        expect(new_game.season).to eq 20122013
+        expect(new_game.away_team_id).to eq 3
+        expect(new_game.home_team_id).to eq 6
+        expect(new_game.away_goals).to eq 2
+        expect(new_game.home_goals).to eq 3        
+        expect(new_game.venue).to eq "Toyota Stadium"
+    end
+      
 end
