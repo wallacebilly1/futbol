9a782219f97c7f95468ed84498ef31e0c71fa57d
Grant Davis <grantdavis303@gmail.com>
2024-03-02 22:45:22 -0700

    Add RSpec Tests for Highest/Lowest & Score Data

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -26,6 +26,22 @@ RSpec.describe Team do
     expect(starter.name).to eq "Atlanta United"
   end
 
-  
+  it '#highest_scoring_visitor outputs correctly' do
+    expect(Team.highest_scoring_visitor).to eq "FC Dallas"
+  end
+
+  it '#lowest_scoring_visitor outputs correctly' do
+    expect(Team.lowest_scoring_visitor).to eq "San Jose Earthquakes"
+  end
+
+  it '#generate_score_data creates correct data' do
+    expect(Team.generate_score_data).to be_a Array
+    expect(Team.generate_score_data[0]).to be_a Hash
+    expect(Team.generate_score_data[0][:team_id]).to be_a Integer
+    expect(Team.generate_score_data[0][:team_name]).to be_a String
+    expect(Team.generate_score_data[0][:team_games_played]).to be_a Integer
+    expect(Team.generate_score_data[0][:team_score_counter]).to be_a Integer
+    expect(Team.generate_score_data[0][:team_average_score_per_game]).to be_a Float
+  end
 
 end
